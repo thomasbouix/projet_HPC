2a8df702f45b866d38d23a295a20694a815ee9ff
Theo <theo.germain@icloud.com>
2020-11-01 16:42:07 +0100

    Ajout fonction compute_fd, passage de routine_fd en 0/255, enregistrement des images diff

--- inc/mouvement.h
+++ inc/mouvement.h
@@ -2,5 +2,6 @@
 #define MOUVEMENT_H
 
 uint8** routine_FrameDifference(uint8** I_t, uint8** I_t_moins_1, int nrl, int nrh, int ncl, int nch, int theta);
+void compute_fd(int num_image, int threshold);
 
 #endif
--- src/mouvement.c
+++ src/mouvement.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "nrutil.h"
 #include "mouvement.h"
 
@@ -13,9 +14,34 @@ uint8** routine_FrameDifference(uint8** I_t, uint8** I_t_moins_1, int nrl, int n
       if(abs(I_t[i][j] - I_t_moins_1[i][j]) < theta)
         E_t[i][j] = (uint8)0;
       else
-        E_t[i][j] = (uint8)1;
+        E_t[i][j] = (uint8)255;
       // O_t[i][j] = (uint8)abs(I_t[i][j] - I_t_moins_1[i][j]);
     }
   }
   return E_t;
+}
+
+
+void compute_fd(int num_image, int threshold){
+  int nrl1, nrh1, ncl1, nch1, nrl2, nrh2, ncl2, nch2;
+  char buff[30];
+
+  //strcpy(path_image_1, base_path);
+  //strcpy(path_image_2, base_path);
+
+  sprintf(buff, "car3/car_3%.3d.pgm", (num_image - 1));
+  uint8** img1 = LoadPGM_ui8matrix(buff, &nrl1, &nrh1, &ncl1, &nch1);
+
+  sprintf(buff, "car3/car_3%.3d.pgm", num_image);
+  uint8** img2 = LoadPGM_ui8matrix(buff, &nrl2, &nrh2, &ncl2, &nch2);
+
+  if( ((nrh1 - nrl1) != (nrh2 - nrl2)) || (nch1 - ncl1) != (nch2 - ncl2)){
+    fprintf(stderr, "***Error : Images must have the same size !\n");
+  }
+
+  uint8** outpout_img = routine_FrameDifference(img2, img1, nrl1, nrh1, ncl1, nch1, threshold);
+  sprintf(buff, "output/fd/diff%.3d.pgm", num_image);
+  SavePGM_ui8matrix(outpout_img, nrl1, nrh1, ncl1, nch1, buff);
+
+  free(outpout_img);
 }
