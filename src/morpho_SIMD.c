002090455d066068fabf4d7bfcadf6e3fa140a5a
Thomas Bouix <thomas.bouix@etu.upmc.fr>
2020-11-14 18:04:36 +0100

    suppression rotation aX cX

--- src/morpho_SIMD.c
+++ src/morpho_SIMD.c
@@ -55,8 +55,8 @@ vbits** erosion_3x3_SIMD(vbits** img_bin, int height, int width)
 
       // ROTATION
       b0 = b1; b1 = b2;
-      a0 = a1; a1 = a2;
-      c0 = c1; c1 = c2;
+      // a0 = a1; a1 = a2;
+      // c0 = c1; c1 = c2;
       aa0 = aa1; aa1 = aa2;
       cc0 = cc1; cc1 = cc2;
     }
@@ -65,11 +65,15 @@ vbits** erosion_3x3_SIMD(vbits** img_bin, int height, int width)
     return m;
   }
 
+  b0 = vec_load(&img_bin_extra_lines[-1][0]);
+  c0 = vec_load(&img_bin_extra_lines[-1][1]);
+  a0 = _mm_bitshift_right(b0, 127);
+
   for(int i = 0; i < height; i++){
     // Gestion de la première colonne de la ligne
-    b0 = vec_load(&img_bin_extra_lines[i-1][0]);
-    c0 = vec_load(&img_bin_extra_lines[i-1][1]);
-    a0 = _mm_bitshift_right(b0, 127);
+
+
+
 
     b1 = vec_load(&img_bin_extra_lines[i-0][0]);
     c1 = vec_load(&img_bin_extra_lines[i-0][1]);
